374056048c4068e33cec43b6b7542822194bf5f7
niranjan323 <niranjanchigulla@gmail.com>
2026-02-23 12:35:49 +0530

    side icons

--- src/Component/Sidebar.tsx
+++ src/Component/Sidebar.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Sidebar.css';
+import homeIcon from '../assets/home.svg';
+import filesIcon from '../assets/files.svg';
 
 interface SidebarProps
 {
@@ -28,10 +30,7 @@ const Sidebar: React.FC<SidebarProps> = ({ onFilesClick, onHomeClick }) =>
                     onClick={handleHomeClick}
                     aria-label="Home"
                 >
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                        <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                    </svg>
+                    <img src={homeIcon} alt="Home" className="sidebar-icon" />
                     <span>Home</span>
                 </button>
 
@@ -41,10 +40,7 @@ const Sidebar: React.FC<SidebarProps> = ({ onFilesClick, onHomeClick }) =>
                     onClick={onFilesClick}
                     aria-label="Files"
                 >
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                        <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path>
-                        <polyline points="13 2 13 9 20 9"></polyline>
-                    </svg>
+                    <img src={filesIcon} alt="Files" className="sidebar-icon" />
                     <span>Files</span>
                 </button>
             </nav>
