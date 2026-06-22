56aa62d9daf5e55dc6fd324d673270058659a3ce
niranjan323 <niranjanchigulla@gmail.com>
2026-06-22 20:50:52 +0530

    nn

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter as BrowserRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import { ElectronProvider } from './context/ElectronContext';
 import { UserDataProvider } from './context/UserDataContext';
+import { UserEmailProvider } from './context/UserEmailContext';
+import { setMsalInstance } from './auth/msalInstance';
 
 const isElectron = typeof window !== 'undefined' && !!window.electronAPI;
 const root = createRoot(document.getElementById('root')!);
@@ -12,35 +14,42 @@ const root = createRoot(document.getElementById('root')!);
 if (isElectron) {
     root.render(
         <StrictMode>
-            <ElectronProvider>
-                <UserDataProvider>
-                    <BrowserRouter>
-                        <App />
-                    </BrowserRouter>
-                </UserDataProvider>
-            </ElectronProvider>
+            <UserEmailProvider value="">
+                <ElectronProvider>
+                    <UserDataProvider>
+                        <HashRouter>
+                            <App />
+                        </HashRouter>
+                    </UserDataProvider>
+                </ElectronProvider>
+            </UserEmailProvider>
         </StrictMode>
     );
 } else {
     import('@azure/msal-browser').then(({ PublicClientApplication }) =>
         import('@azure/msal-react').then(({ MsalProvider }) =>
-            import('./auth/msalConfig').then(({ msalConfig }) => {
-                const msalInstance = new PublicClientApplication(msalConfig);
+            import('./auth/msalConfig').then(({ msalConfig }) =>
+                import('./auth/WebUserEmailProvider').then(({ WebUserEmailProvider }) => {
+                    const msalInstance = new PublicClientApplication(msalConfig);
+                    setMsalInstance(msalInstance);
 
-                root.render(
-                    <StrictMode>
-                        <MsalProvider instance={msalInstance}>
-                            <ElectronProvider>
-                                <UserDataProvider>
-                                    <BrowserRouter>
-                                        <App />
-                                    </BrowserRouter>
-                                </UserDataProvider>
-                            </ElectronProvider>
-                        </MsalProvider>
-                    </StrictMode>
-                );
-            })
+                    root.render(
+                        <StrictMode>
+                            <MsalProvider instance={msalInstance}>
+                                <WebUserEmailProvider>
+                                    <ElectronProvider>
+                                        <UserDataProvider>
+                                            <BrowserRouter>
+                                                <App />
+                                            </BrowserRouter>
+                                        </UserDataProvider>
+                                    </ElectronProvider>
+                                </WebUserEmailProvider>
+                            </MsalProvider>
+                        </StrictMode>
+                    );
+                })
+            )
         )
     );
 }
